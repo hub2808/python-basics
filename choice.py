50f129a85a854d1a5bc8106cce65c55f591c6456
hub2808 <61132988+hub2808@users.noreply.github.com>
2020-02-17 12:28:43 +0530

    Add files via upload

---
+++ choice.py
@@ -0,0 +1,20 @@
+print("select 1 for college")
+print("select 2 for college")
+print("select 3 for vaction")
+print("select 4 for play games")
+print("select 5 for quit")
+
+a=int(input("enter a number"))
+if a==1:
+     print("go to college")
+elif a==2:
+    print("go to vacation or movie")
+elif a==3:
+    print("take rest")
+elif a==4:
+    print("play games")
+elif a==5:
+    print("quit")
+else:
+    print("try another number")
+    
